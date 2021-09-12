1ea36eed6ffaa3e313e87c0567c18e6b732688ec
JB1056 <jblundell1056@gmail.com>
2021-09-13 02:33:16 +1000

    Stylising changes

--- iDU_Crypto_API/idu-coingecko/src/Components/Coin.js
+++ iDU_Crypto_API/idu-coingecko/src/Components/Coin.js
@@ -2,14 +2,19 @@ import React from "react";
 
 function Coin({ coin }) {
     return (
-            /* create list item containing coin info */
-            <li className="coinlist-item">
-                <img className="coinlist-image" src={coin.image} alt={coin.name} />
-                <span className="coinName">{coin.id}</span>
-                <span>${coin.current_price}</span>
-                <span>{coin.market_cap}</span>
-                <span>{coin.market_cap_rank}</span>
-            </li>
+        /* create list item containing coin info */
+        <li className="coinlist-item">
+            <div className="coinHover">
+                <img className="coinlist-image" src={coin.image} alt={coin.id} />
+                <span>{coin.id}</span>
+            </div>
+            <span>{coin.name}</span>
+            <span>${coin.current_price}</span>
+            <div className="coinHover">
+                <p className="marketCap">{coin.market_cap_rank}</p>
+                <span className="marketPosition">Market Cap: {coin.market_cap}</span>
+            </div>
+        </li>
     );
 };
 
